27ffa6328c8b08def556828de6ed7f130f068f50
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 22:46:25 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import sklearn
+import scikit-learn
 
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
@@ -16,7 +16,7 @@ min1 = optionals1.slider("", min_value = 0, max_value = 10)
 st.write(min1)
 col_wor.dropna(inplace=True)
 brandwise = col_wor.groupby(by=['Brand'], as_index=False).agg(lambda x: ", ".join(x))
-vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(max_df=0.95, min_df=0.05)
+vectorizer = scikit-learn.feature_extraction.text.TfidfVectorizer(max_df=0.95, min_df=0.05)
 
 #print(col_wor)
 
