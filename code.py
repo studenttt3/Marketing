48fb7523ff7c607d99223a701abfc9ec30f40204
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 21:11:21 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,8 +6,10 @@ cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
 for i in cat['Category'].unique():
   st.write(i)
-dep_st = cat[cat['Category'] == 'Department Stores']
-dep_st
-for i in dep_st['Brand'].unique():
+beauty = cat[cat['Category'] == 'Beauty Products']
+beauty
+for i in beauty['Brand'].unique():
   st.write(i)
+min1 = optionals1.slider("", min_value = 0, max_value = 10)
+st.write(min1)
 #print(col_wor)
