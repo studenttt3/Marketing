a6c3e4774665d16fc07a88054020d83136705ead
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 20:53:14 +0300

    Update code.py

--- code.py
+++ code.py
@@ -3,5 +3,5 @@ import numpy as np
 
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
-st.write(cat['Category'].unique)
+st.write(cat['Category'].unique())
 #print(col_wor)
