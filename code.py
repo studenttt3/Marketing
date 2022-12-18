1576eee9ade3f8d3d2b727b0ddcd21a8c5cd6b16
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 21:01:08 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,6 +6,6 @@ cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
 for i in cat['Category'].unique():
   st.write(i)
-dep_st = cat[cat['Category'] == 'Department stores']
+dep_st = cat[cat['Category'] == 'Department Stores']
 dep_st
 #print(col_wor)
