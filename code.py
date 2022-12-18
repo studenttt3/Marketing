f9be59a1d14b97a8864178d5fd25971740d02629
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 22:42:33 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import itertools
 
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
@@ -16,7 +15,6 @@ min1 = optionals1.slider("", min_value = 0, max_value = 10)
 st.write(min1)
 col_wor.dropna(inplace=True)
 brandwise = col_wor.groupby(by=['Brand'], as_index=False).agg(lambda x: ", ".join(x))
-all_words_beauty = list(itertools.chain(*col_wor["Words"].apply(lambda x: x.split(", ")).values))
 vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(max_df=0.95, min_df=0.05)
 
 #print(col_wor)
