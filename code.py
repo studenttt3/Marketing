a009184229876f5e8e977af1df3e34eb50555bdf
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 21:28:51 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
@@ -13,4 +14,9 @@ for i in beauty['Brand'].unique():
 optionals1 = st.expander("диапазон", True)
 min1 = optionals1.slider("", min_value = 0, max_value = 10)
 st.write(min1)
+col_wor.dropna(inplace=True)
+brandwise = col_wor.groupby(by=['Brand'], as_index=False).agg(lambda x: ", ".join(x))
+all_words_beauty = list(chain(*words["Words"].apply(lambda x: x.split(", ")).values))
+vectorizer = TfidfVectorizer(max_df=0.95, min_df=0.05)
+
 #print(col_wor)
