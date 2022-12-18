95324f499a9c0341e68395266ca2e5e1c744077a
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-18 22:30:03 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
 for i in cat['Category'].unique():
