bea92aee67926c8bc8a6a2a746d4d4c67d2b4f4b
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 16:14:00 +0300

    Update code.py

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ for i in scores.columns[1:]:
 for i in range(len(scores['index'])):
   brand_rate = 0
   for j in range(len(scores.columns[1:])-1):
-    print(scores.columns[j + 1])
+    st.write(scores.columns[j + 1])
     #print(scores[i, scores.columns[j + 1]])
     #if(type_s[j] == 'Отрицательно'): brand_rate = brand_rate + (10 - scores[i, scores.columns[j + 1]])
     #if(type_s[j] == 'Положительно'): brand_rate = brand_rate + scores[i, scores.columns[j + 1]]
