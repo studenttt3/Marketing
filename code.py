800125ed7f83664824b5a1bc14ecae57a69221bf
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 14:52:58 +0300

    Update code.py

--- code.py
+++ code.py
@@ -6,19 +6,8 @@ import sklearn
 
 cat = pd.read_csv('Categories.csv')
 col_wor = pd.read_csv('collage_words.csv')
-for i in cat['Category'].unique():
-  st.write(i)
-beauty = cat[cat['Category'] == 'Beauty Products']
-beauty
-for i in beauty['Brand'].unique():
-  st.write(i)
-optionals1 = st.expander("диапазон", True)
-min1 = optionals1.slider("", min_value = 0, max_value = 10)
-st.write(min1)
-col_wor.dropna(inplace=True)
-brandwise = col_wor.groupby(by=['Brand'], as_index=False).agg(lambda x: ", ".join(x))
-vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(max_df=0.95, min_df=0.05)
-
+scores = pd.read_csv('Beauty Products.csv')
+scores
 #print(col_wor)
 
 
