0971b955bb916ed98d65e77a1e5e82a9ba59afc2
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 15:28:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -9,7 +9,7 @@ col_wor = pd.read_csv('collage_words.csv')
 scores = pd.read_csv('Beauty Products.csv')
 summ = 0
 type_s = []
-for i in scores.columns:
+for i in scores.columns[1:]:
   type_s.append(st.radio(i, ('Отрицательно','Нейтрально', 'Положительно')))
   #if(type_s == 'Отрицательно'): summ = summ + scores[
     
