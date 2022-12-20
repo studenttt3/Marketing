6d19aca7f3e9716e48502dd2f64e80a6dc744182
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 19:12:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,26 +17,19 @@ which = st.radio('Что важнее для вас при подборе бре
 if(which == 'Быстрота'): n = 5
 if(which == 'Точность'): n = 25
 
-scores
-summ = 0
 type_s = []
 for i in scores.columns[1:n + 1]:
   type_s.append(st.radio(i, ('Отрицательно','Нейтрально', 'Положительно'), index = 1))
-#st.write(len(type_s))
 for i in range(len(scores['index'])):
   brand_rate = 0
   br_sc = list(scores.iloc[i][1:n + 1])
-  #st.write(len(br_sc))
-  #st.write(br_sc)
   for j in range(len(br_sc)):
-    #st.write(int(br_sc[j]))
     if(type_s[j] == 'Отрицательно'): brand_rate = brand_rate + 10 - int(br_sc[j])
     if(type_s[j] == 'Положительно'): brand_rate = brand_rate + int(br_sc[j])
     else: brand_rate = brand_rate
   scores.loc[i, 'rating'] = brand_rate
-#scores
 sorted_sc = scores.sort_values(by='rating')
-sorted_sc
+#sorted_sc
 sorted_sc = sorted_sc.reset_index()
 st.write("5 самых подходящих по характеристикам брендов:")
 for i in range(5):
