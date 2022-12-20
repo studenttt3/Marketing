d31f8a806000a0acbaa8effc090977ac7d77372b
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 16:41:15 +0300

    Update code.py

--- code.py
+++ code.py
@@ -19,3 +19,6 @@ for i in range(len(scores['index'])):
   st.write(br_sc)
   for j in range(len(br_sc)):
     st.write(int(br_sc[j]))
+    if(type_s[j] == 'Отрицательно'): brand_rate = brand_rate + (10 - int(br_sc[j]))
+    if(type_s[j] == 'Положительно'): brand_rate = brand_rate + int(br_sc[j])
+   st.write(brand_rate)
