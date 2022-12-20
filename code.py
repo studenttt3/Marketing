60c0c1c8a95ca022e00244ef66788781a7145907
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-20 19:41:57 +0300

    Update code.py

--- code.py
+++ code.py
@@ -17,6 +17,18 @@ if(cat_ch == 'Clothing products'):
   st.image("https://images.squarespace-cdn.com/content/v1/58c90e50bebafbba1ac44f56/1603290772524-SKGGCSIPRRDR7HSVVA6X/baby+clothes.jpg")
 if(cat_ch == 'Cars'):
   st.image("https://aif-s3.aif.ru/images/016/664/d7018fd8b45e9c280c4f70f0692d400e.jpg")
+if(cat_ch == 'Health products and services'):
+  st.image('https://www.dermascope.com/media/k2/items/cache/5904efb920b46f92d82ad6e67c3d77eb_XL.jpg')
+if(cat_ch == 'Household Products'):
+  st.image('https://www.remodelista.com/wp-content/uploads/2019/01/organized-home-book-sink-detail-matthew-williams-crop-1536x1066.jpg')
+if(cat_ch == 'Food and dining'):
+  st.image('https://www.eatlovesavor.com/wp-content/uploads/2017/03/les-airelles.jpg')
+if(cat_ch == 'Beauty Products'):
+  st.image('http://cdn.shopify.com/s/files/1/0254/1915/3493/products/Untitleddesign_34.png?v=1654186690')
+if(cat_ch == 'Department Stores'):
+  st.image('https://www.japan-guide.com/g20/2072_02.jpg')
+if(cat_ch == 'Home design and Decoration'):
+  st.image('https://media.designcafe.com/wp-content/uploads/2021/05/19174427/minimalist-home-design-ideas.jpg')
 scores = pd.read_csv(cat_ch + '.csv')
 
 st.markdown("В зависимости от располагаемого времени выберите тип подбора. Мы можем сделать более быстрый подбор с меньшим количеством вопросов в опроснике или более точный, но требующий от вас большего количества ответов.")
