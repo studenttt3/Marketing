38b92548f51d8863cc501151bbc179636e9f76bf
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-24 20:32:47 +0300

    Update code.py

--- code.py
+++ code.py
@@ -87,7 +87,7 @@ st.pyplot(fig)
 
 our_vector = pd.read_csv(cat_ch + '_idf.csv')
 fig = plt.figure(figsize = (10,8), dpi = 80)
-df.hist(bins=50, label='Beverages', color='lightsalmon')
+our_vector.hist(bins=50, label=cat_ch, color='lightsalmon')
 st.pyplot(fig)
 
 st.markdown("Но это еще не всё! Мы хотим дать вам еще больше рекомендаций, основываясь на том, какие бренды косметики нравились пользователям, похожим на вас. При этом мы хотим минимизировать ваше недовольство. Пожалуйста, выберите из следующих списков брендов тот, который нравится вам меньше всего.")
