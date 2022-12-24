f8a2758d976a1832a64eb8656c2a38251388c4eb
studenttt3 <105520495+studenttt3@users.noreply.github.com>
2022-12-24 19:55:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -89,9 +89,9 @@ st.markdown("Но это еще не всё! Мы хотим дать вам е�
 ank = pd.read_csv("ank.csv")
 llsstt = []
 for i in range(5):
-  sttr = ank['anketa0']
+  sttr = ank['anketa' + str(i)][0]
   for j in range(4):
-    sttr = sttr + ' ' + ank['anketa' + str(j + 1)]
+    sttr = sttr + ' ' + ank['anketa' + str(i)][j + 1]
   llsstt.append(sttr)
 st.write(llsstt)
 not_like = st.radio('', (ank['anketa0'],ank['anketa1'], ank['anketa2'], ank['anketa3'], ank['anketa4']), index = 1)
